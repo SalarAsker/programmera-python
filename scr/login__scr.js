81107017e655a061f06a11ccab20b62f6523a0f1
SalarAsker <38810077+SalarAsker@users.noreply.github.com>
2023-04-28 10:45:58 +0200

    level1

---
+++ scr/login__scr.js
@@ -0,0 +1,35 @@
+const login__btn = document.getElementById('log__btn');
+console.log(login__btn);
+
+login__btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const u = 'it';
+    const p = 'abc123';
+
+    const user = document.getElementById('user__name').value;
+    const pass = document.getElementById('user__pass').value;
+    console.log(`${user} ${pass}`);
+    if(u === user && p === pass){
+        location.href = `course/startsida.html`;
+    }else{
+        const err = document.querySelector('.error');
+        err.style.display = `block`;
+    }
+})
+
+const user__textbox = document.getElementById('user__name');
+const pass__textbox = document.getElementById('user__pass');
+
+user__textbox.addEventListener('focus', () =>{
+    const err = document.querySelector('.error');
+        err.style.display = `none`;
+});
+
+pass__textbox.addEventListener('focus', () =>{
+    const err = document.querySelector('.error');
+        err.style.display = `none`;
+})
+
+
+
